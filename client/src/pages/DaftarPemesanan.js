37027c6d07edfa0619634d67e70e9fca6a034371
joviakbar9 <joviakbar9@gmail.com>
2022-12-05 20:52:05 +0700

    add scroll daftar pemesanan

--- client/src/pages/DaftarPemesanan.js
+++ client/src/pages/DaftarPemesanan.js
@@ -253,6 +253,7 @@ function DaftarPemesanan() {
         dataSource={searching(pemesananData)}
         bordered
         rowKey="_id"
+        scroll={{x: true}}
       />
 
       {addEditModalVisibility && (
--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
+const URL = "mongodb+srv://jovi:joviakbar890@cluster0.58ewr.mongodb.net/gajayana-pos";
 // mongosh mongodb://%2Fhome%2Fgajayana%2Fmongodb-0.sock
 // const URL = "mongodb://localhost:27017/gajayana-pos";
-const URL = "mongodb+srv://jovi:joviakbar890@cluster0.58ewr.mongodb.net/gajayana-pos";
 
 mongoose.connect(process.env.MONGODB_URI || URL);
 
