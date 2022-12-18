ff479d3991218c3c19a2ad1d445796edc571e7e3
joviakbar9 <joviakbar9@gmail.com>
2022-12-18 09:07:19 +0700

    add validate

--- controllers/masterDataController.js
+++ controllers/masterDataController.js
@@ -1,13 +1,12 @@
-const ItemModel = require('../models/itemsModel');
-const KategoriModel = require('../models/kategoriModel');
-const CustomerModel = require('../models/customerModel');
-const PegawaiModel = require('../models/pegawaiModel');
+const ItemModel = require("../models/itemsModel");
+const KategoriModel = require("../models/kategoriModel");
+const CustomerModel = require("../models/customerModel");
+const PegawaiModel = require("../models/pegawaiModel");
 
 module.exports = {
   getProduk: async (req, res) => {
     try {
-      const items = await ItemModel.find({isArchive:false}).populate('kategori');
-      // console.log(items);
+      const items = await ItemModel.find({ isArchive: false }).populate("kategori");
       res.send(items);
     } catch (error) {
       res.status(400).json(error);
@@ -17,23 +16,26 @@ module.exports = {
     try {
       const newitem = new ItemModel(req.body);
       await newitem.save();
-      res.send('Produk Berhasil Ditambah');
+      res.send("Produk Berhasil Ditambah");
     } catch (error) {
       res.status(400).json(error);
     }
   },
   editProduk: async (req, res) => {
-    try {
+    try {np
       await ItemModel.findOneAndUpdate({ _id: req.body.itemId }, req.body);
-      res.send('Produk Berhasil Diubah');
+      res.send("Produk Berhasil Diubah");
     } catch (error) {
       res.status(400).json(error);
     }
   },
   deleteProduk: async (req, res) => {
     try {
-      await ItemModel.findOneAndUpdate({ _id: req.params.id}, {isArchive: true});
-      res.send('Produk Berhasil Dihapus');
+      await ItemModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { isArchive: true }
+      );
+      res.send("Produk Berhasil Dihapus");
     } catch (error) {
       res.status(400).json(error);
     }
@@ -41,7 +43,7 @@ module.exports = {
 
   getKategori: async (req, res) => {
     try {
-      const kategori = await KategoriModel.find({isArchive:false});
+      const kategori = await KategoriModel.find({ isArchive: false });
       res.send(kategori);
     } catch (error) {
       res.status(400).json(error);
@@ -51,23 +53,26 @@ module.exports = {
     try {
       const newkategori = new KategoriModel(req.body);
       await newkategori.save();
-      res.send('Kategori Baru Berhasil Ditambah');
+      res.send("Kategori Baru Berhasil Ditambah");
     } catch (error) {
       res.status(400).json(error);
     }
   },
   editKategori: async (req, res) => {
     try {
-      await KategoriModel.findOneAndUpdate({ _id: req.body.kategoriId }, req.body);
-      res.send('Kategori Berhasil Diubah');
+      await KategoriModel.findOneAndUpdate({ _id: req.body.kategoriId }, req.body );
+      res.send("Kategori Berhasil Diubah");
     } catch (error) {
       res.status(400).json(error);
     }
   },
   deleteKategori: async (req, res) => {
     try {
-      await KategoriModel.findOneAndUpdate({ _id: req.params.id}, {isArchive: true});
-      res.send('Kategori Berhasil Dihapus');
+      await KategoriModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { isArchive: true }
+      );
+      res.send("Kategori Berhasil Dihapus");
     } catch (error) {
       res.status(400).json(error);
     }
@@ -75,7 +80,7 @@ module.exports = {
 
   getCustomer: async (req, res) => {
     try {
-      const customer = await CustomerModel.find({isArchive:false});
+      const customer = await CustomerModel.find({ isArchive: false });
       res.send(customer);
     } catch (error) {
       res.status(400).json(error);
@@ -85,7 +90,7 @@ module.exports = {
     try {
       const newCustomer = new CustomerModel(req.body);
       await newCustomer.save();
-      res.send('Customer added successfully');
+      res.send("Customer added successfully");
     } catch (error) {
       res.status(400).json(error);
     }
@@ -93,7 +98,7 @@ module.exports = {
   editCustomer: async (req, res) => {
     try {
       await CustomerModel.findOneAndUpdate({ _id: req.body.itemId }, req.body);
-      res.send('Customer updated successfully');
+      res.send("Customer updated successfully");
     } catch (error) {
       res.status(400).json(error);
     }
@@ -101,8 +106,11 @@ module.exports = {
   // router.delete('/:id', async (req, res) => {
   deleteCustomer: async (req, res) => {
     try {
-      await CustomerModel.findOneAndUpdate({ _id: req.params.id}, {isArchive: true});
-      res.send('Customer deleted successfully');
+      await CustomerModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { isArchive: true }
+      );
+      res.send("Customer deleted successfully");
     } catch (error) {
       res.status(400).json(error);
     }
@@ -110,7 +118,7 @@ module.exports = {
 
   getPegawai: async (req, res) => {
     try {
-      const pegawai = await PegawaiModel.find({isArchive:false});
+      const pegawai = await PegawaiModel.find({ isArchive: false });
       res.send(pegawai);
     } catch (error) {
       res.status(400).json(error);
@@ -120,23 +128,29 @@ module.exports = {
     try {
       const newPegawai = new PegawaiModel(req.body);
       await newPegawai.save();
-      res.send('Data Pegawai Berhasil Ditambah');
+      res.send("Data Pegawai Berhasil Ditambah");
     } catch (error) {
       res.status(400).json(error);
     }
   },
   editPegawai: async (req, res) => {
     try {
-      await PegawaiModel.findOneAndUpdate({ _id: req.body.pegawaiId }, req.body);
-      res.send('Data Pegawai Berhasil Diubah');
+      await PegawaiModel.findOneAndUpdate(
+        { _id: req.body.pegawaiId },
+        req.body
+      );
+      res.send("Data Pegawai Berhasil Diubah");
     } catch (error) {
       res.status(400).json(error);
     }
   },
   deletePegawai: async (req, res) => {
     try {
-      await PegawaiModel.findOneAndUpdate({ _id: req.params.id}, {isArchive: true});
-      res.send('Data Pegawai Berhasil Dihapus');
+      await PegawaiModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { isArchive: true }
+      );
+      res.send("Data Pegawai Berhasil Dihapus");
     } catch (error) {
       res.status(400).json(error);
     }
